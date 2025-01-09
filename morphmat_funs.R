98d8eeba311aeb6b71e77fd95b23b5e41ccf20cf
rmk118 <rubykrasnow@gmail.com>
2025-01-09 08:39:57 -0500

    allometry figures

--- morphmat_funs.R
+++ morphmat_funs.R
@@ -1,12 +1,12 @@
 
 
-mytheme <- theme_classic() + #define custom theme for ggplots
+mytheme <- theme_classic() + # define custom theme for ggplots
   theme(
     axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)),
     axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)),
     text = element_text(size = 13))
 
-mytheme_light <- theme_light() + #define custom theme for ggplots
+mytheme_light <- theme_light() + # define custom theme for ggplots
   theme(
     axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)),
     axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)),
